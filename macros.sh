540cc72b205a272169373846718023a40b9cc9cb
Felipe B <felipewbarichello@gmail.com>
2025-04-27 19:06:33 -0300

    Update the macros script

--- macros.sh
+++ macros.sh
@@ -2,3 +2,15 @@
 function api() {
     dotnet $@ --project api
 }
+
+# Run, build, lint or preview the frontend
+# Run this function like the following:
+# - client dev => run the client in dev mode
+# - client build => build the client for production
+# - client lint => run the linter on the client
+# - client preview => preview the client in production mode
+function client() {
+    cd ClientApp
+    npm run $1
+    cd -
+}
