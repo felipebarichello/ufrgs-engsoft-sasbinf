3459915bc35185a3eb39b8c53b8fb5d1ebe9da5e
CaetanoSza <caetanosabadini@gmail.com>
2025-05-01 22:14:31 -0300

    api: post login route added

--- api/src/controllers/APIController.cs
+++ api/src/controllers/APIController.cs
@@ -1,3 +1,4 @@
+using DTO;
 using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
@@ -7,5 +8,14 @@ public class ApiController : ControllerBase
     public IActionResult HealthCheck()
     {
         return Ok(new { message = "api funcionando" });
+    }
+
+    [Route("api/login")]
+    [HttpPost]
+    //after implementing entity framework we change this function to async
+    public ActionResult LoginPost([FromBody] LoginDTO login)
+    {
+        // implement the logic after the entity framework; returns ok now
+        return Ok(new { message = "login approved" });
     }
 }
