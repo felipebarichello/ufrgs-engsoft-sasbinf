2581c070ca7ab9c8142de0369bb3492681fe70de
Felipe B <felipewbarichello@gmail.com>
2025-05-01 21:24:35 -0300

    Remove navigation in App.tsx

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,20 +1,9 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";\
-import { Link } from "react-router-dom";
 import "./App.css";
 import AppRouter from "./routes/AppRouter";
 
 function App() {
   return (
-    <>
-      <nav style={{ padding: '10px', borderBottom: '1px solid #ccc', marginBottom: '10px' }}>
-        <Link to="/" style={{ marginRight: '15px' }}>Home</Link>
-        <Link to="/react">React Example</Link>
-      </nav>
-      
-      <AppRouter />
-    </>
+    <AppRouter />
   );
 }
 
