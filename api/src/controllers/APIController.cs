6d9791bb0ff98d944de533df4be99dfda6de02f5
Felipe B <felipewbarichello@gmail.com>
2025-05-02 19:32:56 -0300

    Simulate async work for later

--- api/src/controllers/APIController.cs
+++ api/src/controllers/APIController.cs
@@ -4,6 +4,12 @@ using Microsoft.AspNetCore.Mvc;
 [ApiController]
 public class ApiController : ControllerBase
 {
+    public readonly IConfiguration _configuration;
+
+    public ApiController(IConfiguration configuration) {
+        _configuration = configuration;
+    }
+
     [HttpGet("api/health")]
     public IActionResult HealthCheck()
     {
@@ -12,8 +18,10 @@ public class ApiController : ControllerBase
     
     [Route("api/login")]
     [HttpPost]
-    public ActionResult LoginPost([FromBody] LoginDTO login)
+    public async Task<ActionResult> LoginPost([FromBody] LoginDTO login)
     {
+        await Task.Delay(100); // Simulate some delay for the login process
+        
         if (login != null && login.user == "qualquercoisa" && login.password == "vazia")
         {
             return Ok(new { message = "Login approved" });
