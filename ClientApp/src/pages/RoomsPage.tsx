25a9f3eb1fb0d1801797506a276923c03e8dd5dc
Felipe B <felipewbarichello@gmail.com>
2025-05-10 15:37:38 -0300

    Readd Restricted to RoomsPage

--- ClientApp/src/pages/RoomsPage.tsx
+++ ClientApp/src/pages/RoomsPage.tsx
@@ -1,16 +1,17 @@
-// import Restricted from "../components/Restricted";
+import Restricted from "../components/Restricted";
 import { useState } from "react";
 import INFLibrary from "../components/InfLibrary";
 import RoomsForm from "../components/RoomsForm";
+
 function RoomsPage() {
   const [availableRooms, setAvailableRooms] = useState<number[]>([]);
   return (
-    // <Restricted>
-    <div className="d-flex justify-content-around" style={{ width: "75vw" }}>
-      <RoomsForm setAvailableRooms={setAvailableRooms} />
-      <INFLibrary available={availableRooms} />
-    </div>
-    // </Restricted>
+    <Restricted>
+      <div className="d-flex justify-content-around" style={{ width: "75vw" }}>
+        <RoomsForm setAvailableRooms={setAvailableRooms} />
+        <INFLibrary available={availableRooms} />
+      </div>
+    </Restricted>
   );
 }
 
