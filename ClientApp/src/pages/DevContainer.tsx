d2882ad0895c8796d471c7d33e1f2a318a6b9e75
Felipe Barichello <felipewbarichello@gmail.com>
2025-05-11 20:14:18 -0300

    client: kill this DevContainer beast again

--- ClientApp/src/pages/DevContainer.tsx
+++
@@ -1,5 +0,0 @@
-import NewPointer from "../components/NewPointer";
-
-export default function DevContainer( ){
-  return <NewPointer enabled={true} roomNumber={4} props={{style: {width: "40%"}}}/>
-}
