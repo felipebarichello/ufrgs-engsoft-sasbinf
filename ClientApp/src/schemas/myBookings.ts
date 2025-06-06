aab7f4ac3856fadd0b2d8882e4eb19915f8e11ab
Felipe B <felipewbarichello@gmail.com>
2025-06-06 19:19:14 -0300

    client: /api/my-bookings response is an array

--- ClientApp/src/schemas/myBookings.ts
+++ ClientApp/src/schemas/myBookings.ts
@@ -1,9 +1,11 @@
 import * as v from 'valibot';
 
-export const MyBookingsResponseSchema = v.object({
-  bookingId: v.number(),
-  roomName: v.string(),
-  startTime: v.string(),
-  endTime: v.string(),
-  status: v.string(),
-});
+export const MyBookingsResponseSchema = v.array(
+  v.object({
+    bookingId: v.number(),
+    roomName: v.string(),
+    startTime: v.string(),
+    endTime: v.string(),
+    status: v.string(),
+  })
+);
